b888e66d2159e340d7796e04d27c23f2c0a360c9
Hung Pham <truonghungit@gmail.com>
2019-08-17 17:18:55 +0700

    Change schematics setting

--- src/app/admin/user/containers/users-page/users-page.component.ts
+++ src/app/admin/user/containers/users-page/users-page.component.ts
@@ -1,19 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-users-page',
   templateUrl: './users-page.component.html',
-  styleUrls: ['./users-page.component.css']
+  styleUrls: ['./users-page.component.scss']
 })
 export class UsersPageComponent implements OnInit {
 
   constructor(
-    private translateService: TranslateService
   ) { }
 
   ngOnInit() {
-    this.translateService.use('en');
   }
 
 }
